0a821eb8f7d9ae7c17a74bf70278bb8b39ede127
Phuong Nguyen <maiphuong.nguyen97@gmail.com>
2023-01-15 23:43:29 -0800

    Modified url in index.js

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		$.ajax({
 			type: 'POST',
 			// url: 'http://localhost:3000/chatbot',
-			url: '${process.env.PUBLIC_URL}/chatbot',
+			url: '/chatbot',
 			data: {
 				message: message
 			},
